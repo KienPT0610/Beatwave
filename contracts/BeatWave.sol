342f36ddd8e81f0456b3a77b330b2f15bfa20aed
kiên phạm <fbkienpt@gmail.com>
2024-06-23 19:44:04 +0700

    feat: add file contract and test

--- contracts/BeatWave.sol
+++ contracts/BeatWave.sol
@@ -2,16 +2,15 @@
 pragma solidity ^0.8.24;
 
 contract BeatWave {
-
     //Struct lưu thông tin beat
     struct Beat {
-        address owner;                  //Người sở hữu
-        string cid;                     //cid trên IPFS 
-        string title;                   //tiêu đề beat  
-        uint256 price;                  //giá bán
-        bool isForSale;                 //trạng thái bán
-        uint256 uploadTimestamp;        //thời gian upload  
-        uint256 numberOfLikes;          //số lượng lượt thích
+        address owner; //Người sở hữu
+        string cid; //cid trên IPFS
+        string title; //tiêu đề beat
+        uint256 price; //giá bán
+        bool isForSale; //trạng thái bán
+        uint256 uploadTimestamp; //thời gian upload
+        uint256 numberOfLikes; //số lượng lượt thích
     }
 
     //Để tính số ID của beat
@@ -27,7 +26,13 @@ contract BeatWave {
     title: tiêu đề của beat
     price: giá beat
     */
-    event BeatUpLoaded(uint256 id, address indexed owner, string cid, string title, uint256 price);
+    event BeatUpLoaded(
+        uint256 id,
+        address indexed owner,
+        string cid,
+        string title,
+        uint256 price
+    );
 
     /*
     Sự kiện thông báo đăng bán beat trên hệ thống
@@ -44,7 +49,12 @@ contract BeatWave {
     to: người sở hữu mới
     value: số tiền mua
     */
-    event BeatSold(uint256 id, address indexed from, address indexed to, uint256 value);
+    event BeatSold(
+        uint256 id,
+        address indexed from,
+        address indexed to,
+        uint256 value
+    );
 
     /*
     Sự kiện chuyển giao quyền sở hữu (không phải mua bán)
@@ -56,11 +66,31 @@ contract BeatWave {
 
     //Kiểm tra người gọi có phải là chủ sở hữu beat không
     modifier onlyOwner(uint256 id) {
-        require(beats[id].owner == msg.sender, "You are not the owner of this beat");
+        require(
+            beats[id].owner == msg.sender,
+            "You are not the owner of this beat"
+        );
         _;
     }
 
-    function uploadBeat(string memory _cid, string memory _title, uint256 _price) public {
+    //Kiếm tra xem có đang bán không
+    modifier isSale(uint256 id) {
+        require(beats[id].isForSale, "This beat is not for sale");
+        _;
+    }
+
+    /*
+    Hàm được gọi khi người dùng upload file lên hệ thống
+    cid: sẽ được lấy từ trên IPFS trả vể
+    title: tiêu đề của beat
+    price: giá
+    Khi gọi xong thì beat sẽ được thêm vào hệ thống beat cá nhân
+    */
+    function uploadBeat(
+        string memory _cid,
+        string memory _title,
+        uint256 _price
+    ) public {
         beatCountId++;
         beats[beatCountId] = Beat({
             owner: msg.sender,
@@ -75,20 +105,51 @@ contract BeatWave {
         emit BeatUpLoaded(beatCountId, msg.sender, _cid, _title, _price);
     }
 
-
-    function listBeatForSale(uint256 _id, uint256 _price) public onlyOwner(_id){
+    /*
+    Hàm được gọi khi người dùng chọn để bán
+    id được lấy từ hệ thống
+    price được lấy từ hệ thống
+    Khi gọi xong thì beat sẽ được thêm vào hệ thống bán 
+    */
+    function listBeatForSale(
+        uint256 _id,
+        uint256 _price
+    ) public onlyOwner(_id) {
         beats[_id].isForSale = true;
         beats[_id].price = _price;
         emit BeatListForSale(_id, msg.sender, _price);
     }
 
-    function likeBeat(uint256 _id) public {
-        beats[_id].numberOfLikes += 1;
+    /*
+    Người bán không muốn bán nữa hàm này sẽ được gọi
+    Khi gọi xong trạng thái bán được gỡ
+    Hệ thống sẽ cập nhật và xóa beat khỏi hệ thống bán
+    */
+    function deleteBeatForSale(uint _id) public onlyOwner(_id) {
+        beats[_id].isForSale = false;
     }
 
+    /*
+    Hàm được gọi khi người dùng bấm like beat
+    Nếu người dùng chưa like thì numberOfLikes + 1
+    Nếu người dùng đã like thì sẽ thảnh bỏ like - 1
+    */
+    function likeBeat(uint256 _id, bool statusLiked) public isSale(_id) {
+        if (!statusLiked) {
+            beats[_id].numberOfLikes += 1;
+        } else {
+            beats[_id].numberOfLikes -= 1;
+        }
+    }
 
-    function buyBeat(uint256 _id) public payable {
-        require(beats[_id].isForSale, "This beat is not for sale");
+    /*
+    Hàm được gọi khi người mua bấm vào mua beat
+    id được lấy từ hệ thống
+    chuyển tiền cho người bán
+    chuyển quyền sở hữu cho người mua
+    đánh trạng thái đang bán thành false
+    */
+    function buyBeat(uint256 _id) public payable isSale(_id) {
         require(beats[_id].price == msg.value, "Incorrect Price");
 
         address payable owner = payable(beats[_id].owner);
@@ -97,11 +158,17 @@ contract BeatWave {
 
         beats[_id].owner = msg.sender;
         beats[_id].isForSale = false;
-        
+
         emit BeatSold(_id, owner, msg.sender, msg.value);
     }
 
-    function transferOwner(uint256 _id, address newOwner) public {
+    /*
+    Người sở hữu có thể chuyển quyên sở hữu cho bất kì ai
+    */
+    function transferOwner(
+        uint256 _id,
+        address newOwner
+    ) public onlyOwner(_id) {
         address owner = beats[_id].owner;
         beats[_id].owner = newOwner;
         emit Transfer(_id, owner, newOwner);
